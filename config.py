9244a2908a22f78d81ad60c97ebbd72901767d1b
Андрей <andreykaravaev640@gmail.com>
2025-05-10 21:42:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv
 import os
+load_dotenv()
+
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SECRET_KEY = os.getenv('SQLALCHEMY_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///tasks.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
